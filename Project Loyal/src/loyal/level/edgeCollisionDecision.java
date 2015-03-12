ebcf741ab1d5f4adbdbc02289a1e412437ec4c1e
nocturnhabeo <alexddupree@gmail.com>
2015-03-12 13:44:54 -0700

    Adding code for AI and Controller

--- Project Loyal/src/loyal/level/edgeCollisionDecision.java
+++ Project Loyal/src/loyal/level/edgeCollisionDecision.java
@@ -20,19 +20,17 @@ public class edgeCollisionDecision implements Decision {
 
 	@Override
 	public void update() {
-		game.level.music.stop();		
-		game.level = Loyal.levels[levelWatched.newLevelIdentifier(entityWatchedX, entityWatchedY)];
-
+		game.level.music.stop();
+		game.level = Loyal.levels[levelWatched.newLevelIdentifier(
+				entityWatchedX, entityWatchedY)];
 
 		if (!game.level.hasBeenPlayed) {
-			MapPlayer player = new MapPlayer(this.game.level, 0, 0,
-					this.input, this.game);
+			MapPlayer player = new MapPlayer(this.game.level, 0, 0, this.input,
+					this.game);
 			game.level.addEntity(0, player);
 		}
-		
-		
-		if(game.level.getEntitySize() == 1 && game.level.getId() == 2)
-		{
+
+		if (game.level.getEntitySize() == 1 && game.level.getId() == 2) {
 			addMonsters();
 		}
 		game.level.music.loop();
@@ -51,30 +49,27 @@ public class edgeCollisionDecision implements Decision {
 		this.entityWatchedX = levelWatched.entities.get(0).getX();
 		this.entityWatchedY = levelWatched.entities.get(0).getY();
 		this.input = game.input;
-		
 	}
-	
-	public void addMonsters()
-	{
+
+	public void addMonsters() {
 		Random random = new Random();
-		
+
 		int MonsterX = random.nextInt(399);
 		int MonsterY = random.nextInt(399);
-		for(int i = 0; i<100; i++)
-		{
-			Tile tile = game.level.getTile(MonsterX,MonsterY);
-			while(tile.isSolid()==true || game.level.tiles[MonsterX+MonsterY*game.level.width] == 3)
-			{
+		for (int i = 0; i < 100; i++) {
+			Tile tile = game.level.getTile(MonsterX, MonsterY);
+			while (tile.isSolid() == true
+					|| game.level.tiles[MonsterX + MonsterY * game.level.width] == 3) {
 				MonsterX = random.nextInt(399);
 				MonsterY = random.nextInt(399);
-				tile = game.level.getTile(MonsterX,MonsterY);
+				tile = game.level.getTile(MonsterX, MonsterY);
 			}
-			MapMoblin monster = new MapMoblin(this.game.level, MonsterX*8, MonsterY*8, 1, 64);
+			MapMoblin monster = new MapMoblin(this.game.level, MonsterX * 8,
+					MonsterY * 8, 1, 64);
 			game.level.addEntity(monster);
 			MonsterX = random.nextInt(399);
 			MonsterY = random.nextInt(399);
 		}
 	}
-	
 
 }
