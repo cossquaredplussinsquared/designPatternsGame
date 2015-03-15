50141eb469de34920881642a2cb74bc7438ced86
Paul <galahadp@hotmail.com>
2015-03-15 16:00:57 -0700

    battle level

---
+++ Project Loyal/src/loyal/level/BattleLevel.java
@@ -0,0 +1,14 @@
+package loyal.level;
+
+import loyal.Sound;
+
+public class BattleLevel extends Level
+{
+
+	public BattleLevel(int id, String imagePath, Sound music)
+	{
+		super(id, imagePath, music);
+		
+	}
+
+}
