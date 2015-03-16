0f117ec8c2539ea9a6e24c8f378565d236553fbb
jeffclarkk <jeffrey.clark@eagles.ewu.edu>
2015-03-16 08:23:52 -0700

    fixed battle party stuff

--- Project Loyal/src/loyal/entities/GeneratorOfParties.java
+++ Project Loyal/src/loyal/entities/GeneratorOfParties.java
@@ -8,7 +8,7 @@ import loyal.Battle.Characters.PlayingCharacter;
 import loyal.Battle.Characters.CharacterState.Stat;
 
 public class GeneratorOfParties {
-	static private ArrayList<PlayingCharacter> party, enemy;
+	private ArrayList<PlayingCharacter> party, enemy;
 	PlayingCharacter hunter;
 	PlayingCharacter warrior;
 	PlayingCharacter cleric;
@@ -21,24 +21,28 @@ public class GeneratorOfParties {
 	}
 
 	public ArrayList<PlayingCharacter> generateParty() {
-		if (this.party == null) {
-			this.hunter = new Hunter("Hunt", CharacterType.HUNTER);
-			this.warrior = new Warrior("War", CharacterType.WARRIOR);
-			this.cleric = new Cleric("Cler", CharacterType.CLERIC);
-			this.wizard = new Wizard("Wiz", CharacterType.WIZZARD);
-			this.party.add(this.hunter);
-			this.party.add(this.warrior);
-			this.party.add(this.cleric);
-			this.party.add(this.wizard);
+		if (party.isEmpty())
+		{
+			party.add(new Hunter("Hunt", CharacterType.HUNTER));
+			party.add(new Warrior("War", CharacterType.WARRIOR));
+			party.add(new Cleric("Cler", CharacterType.CLERIC));
+			party.add(new Wizard("Wiz", CharacterType.WIZZARD));
 		}
-		return party;
+//			this.party.add(this.hunter);
+//			this.party.add(this.warrior);
+//			this.party.add(this.cleric);
+//			this.party.add(this.wizard);
+		return this.party;
 	}
 	
 	public ArrayList<PlayingCharacter> generateEnemyParty(){
-		enemy.add( new Hunter("enemy hunter", CharacterType.HUNTER));
-		enemy.add( new Warrior("enemy warrior", CharacterType.WARRIOR));
-		enemy.add( new Cleric("enemy cleric", CharacterType.CLERIC));
-		enemy.add(  new Wizard("enemy wizard", CharacterType.WIZZARD));
+		if (enemy.isEmpty())
+		{
+			enemy.add( new Hunter("enemy hunter", CharacterType.HUNTER));
+			enemy.add( new Warrior("enemy warrior", CharacterType.WARRIOR));
+			enemy.add( new Cleric("enemy cleric", CharacterType.CLERIC));
+			enemy.add(  new Wizard("enemy wizard", CharacterType.WIZZARD));
+		}
 		return this.enemy;
 		
 		
